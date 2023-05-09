28cd9eb64404000582bedc8128d0305c6c0f854d
Nexvaulus <moehammad.fikr@gmail.com>
2023-05-09 20:52:20 +0700

    Second commit

--- server.js
+++ server.js
@@ -1,15 +1,68 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
-  let body = [];
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("X-Powered-By", "NodeJS");
 
-  request.on("data", (chunk) => {
-    body.push(chunk);
-  });
+  const { method, url } = request;
 
-  request.on("end", () => {
-    body = Buffer.concat(body).toString();
-  });
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "Ini adalah homepage",
+        })
+      );
+    } else {
+      response.statusCode = 400;
+      response.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses dengan ${method} request`,
+        })
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end(
+        JSON.stringify({
+          message: "Halo! Ini adalah halaman about",
+        })
+      );
+    } else if (method === "POST") {
+      let body = [];
+
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.statusCode = 200;
+        response.end(
+          JSON.stringify({
+            message: `Halo, ${name}! Ini adalah halaman about`,
+          })
+        );
+      });
+    } else {
+      response.statusCode = 400;
+      response.end(
+        JSON.stringify({
+          message: `Halaman tidak dapat diakses menggunakan ${method}, request`,
+        })
+      );
+    }
+  } else {
+    response.statusCode = 404;
+    response.end(
+      JSON.stringify({
+        message: "Halaman tidak ditemukan!",
+      })
+    );
+  }
 };
 
 const server = http.createServer(requestListener);
